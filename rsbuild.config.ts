bfb71660b1da52ec373fceaa7965713969b77936
Shashank Raju <shashank@vrittiai.com>
2025-09-28 02:01:11 +0530

    Refactor code structure and remove redundant sections for improved readability and maintainability

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,17 @@
+import { pluginModuleFederation } from '@module-federation/rsbuild-plugin';
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
-  plugins: [pluginReact()],
+  plugins: [
+    pluginReact(),
+    pluginModuleFederation({
+      name: 'mf_web_nexus',
+      shared: {
+        react: { singleton: true, requiredVersion: '^19.0.0' },
+        'react-dom': { singleton: true },
+        'react-router-dom': { singleton: true },
+      },
+    }),
+  ],
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 const App = () => {
   return (
-    <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
-    </div>
+    <BrowserRouter>
+      <div className='content'>
+        <h1>Rsbuild with React</h1>
+        <p>Start building amazing things with Rsbuild.</p>
+        <Routes>
+          <Route path='/' element={<div>Home Page</div>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
 
